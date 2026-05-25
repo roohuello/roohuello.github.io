f76815fa5c39f2939e7992f1f3ee5669876d9229
Rogelio Dimakiling Jr. <rogeliodimakiling@gmail.com>
2026-05-25 14:25:27 +0800

    update base path for root domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://roohuello.github.io',
-  base: '/portfolio/',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   },
